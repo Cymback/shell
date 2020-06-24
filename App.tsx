81f0b58099154e3bb080c6ee50067dc8fdcc1969
cymback <caspergrozzsteam@gmail.com>
2020-06-24 15:25:35 +0200

    Cleanup and styling

--- App.tsx
+++ App.tsx
@@ -1,51 +1,26 @@
-import {StyleSheet, View, TouchableOpacity, Text} from 'react-native';
+import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 import * as React from 'react';
 
 export default function App() {
 
-  async function pushApprovedV2() {
-
-    const url = 'https://testmobile.iot.care/app/workspace/changesignin2fa' + status;
-    try {
-      const response = await fetch(url, {
-        method: 'POST',
-        mode: 'cors',
-        headers: {
-          'content-type': 'application/json',
-          authorization: 'b8cad92bc52896eadb1bf265b785f90f'
-        }
-      });
-      if (response.ok) {
-        console.log('Got what we wanted' + response.status);
-      } else {
-        console.log('Failed to get what we wanted, got status: ' + response.status);
-      }
-      return "okay";
-    } catch (e) {
-      console.log('An actual error!');
-      return "network error";
-    }
+  function pushApproved() {
+    console.log('Approved Login message');
+    fetch('https://testmobile.iot.care/app/workspace/changesignin2fa', {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json',
+        authorization: 'b8cad92bc52896eadb1bf265b785f90f'
+      },
+      body: JSON.stringify(
+          {
+            plat4muserid: 'sac.steen.amelungs',
+            signin: '1',
+          })
+    });
   }
 
-
-  // function pushApproved() {
-  //   console.log('Approved Login message');
-  //   fetch('https://testmobile.iot.care/app/workspace/changesignin2fa', {
-  //     method: 'POST',
-  //     headers: {
-  //       'content-type': 'application/json',
-  //       authorization: 'b8cad92bc52896eadb1bf265b785f90f'
-  //     },
-  //     body: JSON.stringify(
-  //         {
-  //           plat4muserid: 'sac.steen.amelungs',
-  //           signin: '1',
-  //         })
-  //   });
-  // }
-
   function pushDeclined() {
-    console.log('Declined login request')
+    console.log('Declined login request');
     fetch('https://testmobile.iot.care/app/workspace/changesignin2fa', {
       method: 'POST',
       headers: {
@@ -63,7 +38,7 @@ export default function App() {
   return (
       <View style={styles.container}>
         <View style={styles.buttonContainer}>
-          <TouchableOpacity style={{height: 40, width: 65, margin: 20}} onPress={pushApprovedV2}>
+          <TouchableOpacity style={{height: 40, width: 65, margin: 20}} onPress={pushApproved}>
             <Text style={styles.TextStyle}>Approve</Text>
           </TouchableOpacity>
         </View>
@@ -77,19 +52,20 @@ export default function App() {
 }
 
 const styles = StyleSheet.create(
-    {container: {
-      flex: 1,
+    {
+      container: {
+        flex: 1,
         justifyContent: 'center',
         padding: 20,
         flexDirection: 'row',
         alignItems: 'center',
       },
       buttonContainer: {
-      margin: 10,
+        margin: 10,
         backgroundColor: '#6495ED',
       },
       TextStyle: {
-      color: '#fff',
+        color: '#fff',
         textAlign: 'center',
         fontSize: 16,
       }
